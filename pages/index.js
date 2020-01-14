9e90a5821a09bd1a533fc1b9d906656d14855520
Kenneth <sally@sallyde-MacBook-Air.local>
2020-01-14 00:38:02 -0800

    create an index page

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+const Hello = () => {
+  return (
+    <div>
+      <h1>Hello page here !!!</h1>
+    </div>
+  )
+};
+
+export default Hello;
+//this is the root page;
+//create route for each page  index page === "/" route
